f1d217ef2273368bc9bc9055ada6e220300b8bcd
Marta Castrillo <112553001+martscastrillo@users.noreply.github.com>
2023-06-12 13:16:21 +0200

    Create singly.js

---
+++ singly.js
@@ -0,0 +1,36 @@
+1-- > 2-- > 3-- > 4-- > 5-- > null;
+
+let singlyLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: null,
+        },
+      },
+    },
+  },
+};
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class MySinglyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+
+    this.length = 1;
+  }
+}
